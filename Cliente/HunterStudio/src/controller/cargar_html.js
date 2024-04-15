07eb1d01bc21e596e99cf1cb14876725b2258240
Mike <57753913+Terremix@users.noreply.github.com>
2024-04-15 13:34:01 +0200

    Cambios varios

--- Cliente/HunterStudio/src/controller/cargar_html.js
+++ Cliente/HunterStudio/src/controller/cargar_html.js
@@ -1,27 +1,34 @@
-var enlaces = document.querySelectorAll('#menu a');
-
-// Asigna el evento clic a cada enlace
-
-enlaces.forEach(function(enlace) {
-    enlace.addEventListener('click', function(event) {
-        // Evita que el enlace siga el enlace predeterminado
-        event.preventDefault();
-
-        // Carga el contenido dinámico
-        cargarContenido(this.getAttribute('href'));
-    });
-});
-
-function cargarContenido(url) {
+function cargarContenido(url, contenedor) {
     // Utiliza fetch para obtener el contenido del archivo HTML
     fetch(url)
         .then(response => response.text())
         .then(data => {
             // Inserta el contenido en el contenedor
-            document.getElementById('contenedor').innerHTML = data;
+            document.getElementById(contenedor).innerHTML = data;
         })
-        
         .catch(error => console.error('Error al cargar el contenido:', error));
+}
+
+function configurarLista(enlaceClicado){
+
+    var asideElement = enlaceClicado.closest(".menu-leteral");
+    var listItems = asideElement.querySelectorAll('ul li');
+
+    // Si no se encuentra ningún elemento <aside> con la clase especificada, la función termina
+    if (!asideElement) {
+        console.error("No se encontró ningún elemento con la clase 'menu-leteral'");
+        return;
+    }
+
+    listItems.forEach(function(li) {
+        var enlace = li.querySelector('a');
+        // Operaciones en cada elemento li
+        enlace.removeAttribute('id');
+        console.log(li);
+    });
+
+    // Resaltamos el elemento clicado
+    enlaceClicado.setAttribute('id', 'element-clicked');
 }
 
 function configurarBorde(enlaceClicado){
@@ -32,10 +39,12 @@ function configurarBorde(enlaceClicado){
     listItems.forEach(function(li) {
         // Operaciones en cada elemento li
         li.style.setProperty("border-bottom-color", "black");
+        li.style.setProperty("background-color", "gray");
     });
 
     var liPadre = enlaceClicado.parentNode;
 
     // Ponemos el borde del fondo en transparente
     liPadre.style.setProperty("border-bottom-color", "gray");
+    liPadre.style.setProperty("background-color", "lightgreen");
 }
