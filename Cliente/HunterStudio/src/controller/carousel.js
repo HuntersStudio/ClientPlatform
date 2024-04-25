62b3cd8f8bba85e41f099123f9b6bbc79e26a0a4
Mike <57753913+Terremix@users.noreply.github.com>
2024-04-26 00:39:36 +0200

    carousel

--- Cliente/HunterStudio/src/controller/carousel.js
+++ Cliente/HunterStudio/src/controller/carousel.js
@@ -1,6 +1,58 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const elementosCarousel = document.querySelectorAll('.carousel'); // Sacamos los elementos del div 'carousel'
-    M.Carousel.init(elementosCarousel, { // Aqui se llama al script de internet para llamar al metodo M.Carousel
-        duration: 150 // 
-    });
-});
+const carousel_controller = document.querySelectorAll('.carousel');
+const carousel_controls_controller = document.querySelectorAll('.carousel-controls');
+const carousel_controls = ['prev', 'next'];
+const carousel_items = document.querySelectorAll('.item-carousel');
+
+class Carousel {
+
+    constructor(container, items, controls) {
+        this.carouselContainer = container;
+        this.carouselControls = controls;       
+        this.carouselArray = [...items];
+    }
+
+    updateCarousel() {
+        this.carouselArray.forEach(el => {
+            el.classList.remove('.item-carousel-1');
+            el.classList.remove('.item-carousel-2');
+            el.classList.remove('.item-carousel-3');
+            el.classList.remove('.item-carousel-4');
+            el.classList.remove('.item-carousel-5');            
+        });
+
+        this.carouselArray.slice(0, 5).forEach((el, i) => {
+            el.classList.add('.item-carousel-${i+1}');
+        });
+    }
+
+    setCurrentState(direction) {
+        if(direction.className == 'carousel-controls-prev') {
+            this.carouselArray.unshift(this.carouselArray.pop());
+        } else {
+            this.carouselArray.push(this.carouselArray.shift());
+        }
+        this.updateCarousel();
+    }
+
+    setControlls() {
+        this.carouselControls.forEach(control => {
+            carousel_controls_controller.appendChild(document.createElement('button')).className = 'carousel-controls-${control}';
+            document.querySelector('carousel-controls-${control}').innerText = control;
+        });
+    }
+
+    useControls() {
+        const triggers = [...carousel_controls_controller.childNodes];
+        triggers.forEach(control => {
+            control.addEventListener('click', e => {
+                e.preventDefault();
+                this.setCurrentState(control);
+            });
+        });
+    }
+}
+
+const carousel_main = new Carousel(carousel_controller, carousel_items, carousel_controls);
+
+carousel_main.setControlls();
+carousel_main.useControls();
--- Cliente/HunterStudio/src/controller/elementos.js
+++ Cliente/HunterStudio/src/controller/elementos.js
@@ -1,18 +1,31 @@
-let sliderImages = document.querySelectorAll('.slider-img');
+(function() {
+    // Variable que almacena las imágenes del slider
+    let sliderImages;
 
-sliderImages.forEach(function(sliderImage) {
+    // Función para inicializar el slider
+    function initializeSlider() {
+        // Verifica si sliderImages aún no ha sido inicializada
+        if (!sliderImages) {
+            // Obtiene las imágenes del slider
+            sliderImages = document.querySelectorAll('.slider-img');
 
-    // Añade un manejador de eventos de clic a cada imagen
-    sliderImage.addEventListener('click', function() {
-        
-        // Quita la clase 'active' de todas las imágenes del slider excepto la imagen clicada
-        sliderImages.forEach(function(img) {
-            if (img !== sliderImage) {
-                img.classList.remove('active');
-            }
-        });
+            // Asigna el manejador de eventos de clic a cada imagen
+            sliderImages.forEach(function(sliderImage) {
+                sliderImage.addEventListener('click', function() {
+                    // Quita la clase 'active' de todas las imágenes del slider excepto la imagen clicada
+                    sliderImages.forEach(function(img) {
+                        if (img !== sliderImage) {
+                            img.classList.remove('active');
+                        }
+                    });
 
-        // Alterna la clase 'active' en la imagen clicada
-        sliderImage.classList.toggle('active');
-    });
-});
+                    // Alterna la clase 'active' en la imagen clicada
+                    sliderImage.classList.toggle('active');
+                });
+            });
+        }
+    }
+
+    // Llama a la función para inicializar el slider
+    initializeSlider();
+})();
