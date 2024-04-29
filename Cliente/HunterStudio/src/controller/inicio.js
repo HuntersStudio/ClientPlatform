651e28034f1ab03f95a29b9d1450c3e6b852571c
Marcos Santos <57753913+Terremix@users.noreply.github.com>
2024-04-29 18:02:56 +0200

    carousel
    casi terminado

--- Cliente/HunterStudio/src/controller/inicio.js
+++ Cliente/HunterStudio/src/controller/inicio.js
@@ -1,53 +1,57 @@
-let items = document.querySelectorAll('.slider .list .item');
-let next = document.getElementById('next');
-let prev = document.getElementById('prev');
-let thumbnails = document.querySelectorAll('.thumbnail .item');
-let contItem = items.length;
-let itemActive = 0;
-   
-next.onclick = function(){
-    itemActive++;
-    if(itemActive >= contItem){
-        itemActive = 0;
-    }
-    showSlider();
-}
+(function() {
+    let items = document.querySelectorAll('.slider .list .item');
+    let next = document.getElementById('next');
+    let prev = document.getElementById('prev');
+    let thumbnails = document.querySelectorAll('.thumbnail .item');
+    let contItem = items.length;
+    let itemActive = 0;
 
-prev.onclick = function(){
-    itemActive--;
-    if(itemActive < 0){
-        itemActive = contItem -1;
-    }
-    showSlider();
-}
 
-let refreshInterval = setInterval(() => {
-    next.click();
-}, 5000);
-
-function showSlider() {
-
-    var elemento = document.querySelector(".slider .list .item.active");
-
-    if(elemento != null){
-        let itemActiveOld = document.querySelector('.slider .list .item.active');
-        let thumbnailActiveOld = document.querySelector('.thumbnail .item.active');
-        itemActiveOld.classList.remove('active');
-        thumbnailActiveOld.classList.remove('active');
-        
-        // Añade la clase 'active' al ítem y miniatura activos
-        items[itemActive].classList.add('active');
-        thumbnails[itemActive].classList.add('active');
-    }
-    clearInterval(refreshInterval);
-    refreshInterval = setInterval(() => {
-        next.click();
-    }, 8000);
-}
-
-thumbnails.forEach((thumbnail, index) => {
-    thumbnail.addEventListener('click', () => {
-        itemActive = index;
+    next.onclick = function(){
+        itemActive++;
+        if(itemActive >= contItem){
+            itemActive = 0;
+        }
         showSlider();
-    });
+    }
+
+    prev.onclick = function(){
+        itemActive--;
+        if(itemActive < 0){
+            itemActive = contItem -1;
+        }
+        showSlider();
+    }
+
+    let refreshInterval = setInterval(() => {
+        next.click();
+    }, 5000);
+
+    function showSlider() {
+
+        var elemento = document.querySelector(".slider .list .item.active");
+
+        if(elemento != null){
+            let itemActiveOld = document.querySelector('.slider .list .item.active');
+            let thumbnailActiveOld = document.querySelector('.thumbnail .item.active');
+            itemActiveOld.classList.remove('active');
+            thumbnailActiveOld.classList.remove('active');
+            
+            // Añade la clase 'active' al ítem y miniatura activos
+            items[itemActive].classList.add('active');
+            thumbnails[itemActive].classList.add('active');
+        }
+        clearInterval(refreshInterval);
+        refreshInterval = setInterval(() => {
+            next.click();
+        }, 8000);
+    }
+
+    thumbnails.forEach((thumbnail, index) => {
+        thumbnail.addEventListener('click', () => {
+            itemActive = index;
+            showSlider();
+        });
 });
+
+})();
