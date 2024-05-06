1cc0021cfddd238d6a7ba96b91b9e602101da77e
Marcos Santos <57753913+Terremix@users.noreply.github.com>
2024-05-06 16:34:35 +0200

    fondo
    POR FIN
    un fondo en condiciones, ahora solo falta poner bien colores y demas que se lo dejare al jefe,
    si algun dia algun psicopata se pone a leer estas notas puede ser gracioso

--- Cliente/HunterStudio/src/controller/fondo.js
+++ Cliente/HunterStudio/src/controller/fondo.js
@@ -1,12 +1,23 @@
-const generate_space_layer = () => {
-    const layer = [];
-    const total_starts = 200;
+document.addEventListener("DOMContentLoaded", function() {
+    const COLORS = ["#fff2", "#fff4", "#fff7", "#fffc"];
 
-    for (let i = 0; i < total_starts; i++){
-        const x = Math.floor(Math.random() *100);
-        const y = Math.floor(Math.random() *100);
-        layer.push('${x} ${y} 0 white');
-    }
+    const generate_space_layer = (size, selector, total_starts, duration) => {
+        const layer = [];
 
-    const container = document.
-}
+        for (let i = 0; i < total_starts; i++){
+            const color = COLORS[Math.floor(Math.random() * COLORS.length)];
+            const x = Math.floor(Math.random() * 100);
+            const y = Math.floor(Math.random() * 100);
+            layer.push(`${x}vw ${y}vh 0 ${color}, ${x}vw ${y + 100}vh 0 ${color}`);
+        }
+
+        const container = document.querySelector(selector);
+        container.style.setProperty("--space-layer", layer.join(","));
+        container.style.setProperty("--size", size);
+        container.style.setProperty("--duration", duration);
+    };
+
+    generate_space_layer("1px", ".space-1", 200, "25s");
+    generate_space_layer("2px", ".space-2", 100, "20s");
+    generate_space_layer("4px", ".space-3", 25, "15s");
+});
