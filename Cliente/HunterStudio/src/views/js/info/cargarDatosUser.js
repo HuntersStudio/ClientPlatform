737ee9125dd2cd85a3973eefaa6b40fd146074a7
Mike <57753913+Terremix@users.noreply.github.com>
2024-06-05 23:28:56 +0200

    JS
    recuperar datos del usuario para mostrar informacion

--- Cliente/HunterStudio/src/views/js/info/cargarDatosUser.js
+++ Cliente/HunterStudio/src/views/js/info/cargarDatosUser.js
@@ -1,36 +1,37 @@
+(function () {
+  const campoUser = document.querySelector('.user_name');
+  const campoEmail = document.querySelector('.user_mail');
+  const categoria = document.querySelector('.num_categoria');
+  const fecha = document.querySelector('.fecha');
 
-const campoUser = document.querySelector('.user_name');
-const campoEmail = document.querySelector('.user_mail');
-const categoria = document.querySelector('.num_categoria');
-const fecha = document.querySelector('.fecha');
+  const token = sessionStorage.getItem('token');
 
-const token = sessionStorage.getItem('token');
+  if (token) {
+    const userUrl = 'http://localhost:8004/service/getUserDetails';
 
-if (token) {
-  const userUrl = 'http://localhost:8004/service/getUserDetails';
-
-  fetch(userUrl, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer ' + token
-    }
-  })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('La respuesta de la red no fue correcta');
+    fetch(userUrl, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token
       }
-      return response.json();
     })
-    .then(data => {
-      campoUser.textContent = data.userName;
-      campoEmail.textContent = data.email;
-      categoria.textContent = data.categoryLevel;
-      fecha.textContent = data.registrationDate;
-    })
-    .catch(error => {
-      console.error('Error al obtener los detalles del usuario:', error);
-    });
-} else {
-  console.error('No hay token en sessionStorage');
-}
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('La respuesta de la red no fue correcta');
+        }
+        return response.json();
+      })
+      .then(data => {
+        campoUser.textContent = data.userName;
+        campoEmail.textContent = data.email;
+        categoria.textContent = data.categoryLevel;
+        fecha.textContent = data.registrationDate;
+      })
+      .catch(error => {
+        console.error('Error al obtener los detalles del usuario:', error);
+      });
+  } else {
+    console.error('No hay token en sessionStorage');
+  }
+})();
